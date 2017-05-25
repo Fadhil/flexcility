b25d52a864c422d6d7f4137ac008d6aa84366ef5
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-25 11:53:26 +0800

    Use uuid to get specific user in UserController

--- apps/flexcility_web/lib/flexcility_web/controllers/user_controller.ex
+++ apps/flexcility_web/lib/flexcility_web/controllers/user_controller.ex
@@ -12,10 +12,10 @@ defmodule Flexcility.Web.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+    with {:ok, user} <- Accounts.create_user(user_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", user_path(conn, :show, user))
+      |> put_resp_header("location", user_path(conn, :show, user.uuid))
       |> render("show.json", user: user)
     end
   end
