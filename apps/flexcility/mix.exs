7517abd2f66017c199f706d5f7b10725cd06a351
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-26 19:16:26 +0800

    Bump version to 0.3.2

--- apps/flexcility/mix.exs
+++ apps/flexcility/mix.exs
@@ -3,7 +3,7 @@ defmodule Flexcility.Mixfile do
 
   def project do
     [app: :flexcility,
-     version: "0.2.3",
+     version: "0.3.2",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
--- apps/flexcility_web/mix.exs
+++ apps/flexcility_web/mix.exs
@@ -3,7 +3,7 @@ defmodule Flexcility.Web.Mixfile do
 
   def project do
     [app: :flexcility_web,
-     version: "0.2.3",
+     version: "0.3.2",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
