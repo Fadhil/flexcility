c61be2194420a219e94328bb48f43a9b5fdb5658
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-06-12 19:30:23 +0800

    Add Bolt.Sips config for test env

--- apps/flexcility/config/test.exs
+++ apps/flexcility/config/test.exs
@@ -8,3 +8,10 @@ config :flexcility, Flexcility.Repo,
   database: "flexcility_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :bolt_sips, Bolt,
+  hostname: 'localhost',
+  basic_auth: [username: "neo4j", password: "hentamsajaLah"],
+  port: 7687,
+  pool_size: 5,
+  max_overflow: 1
