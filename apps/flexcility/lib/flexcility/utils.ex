6a34c66b8e3067ea0588bd39859fe34374ec5ef4
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-12 22:40:44 +0800

    Tabs to spaces for Flexcility.Utils

--- apps/flexcility/lib/flexcility/utils.ex
+++ apps/flexcility/lib/flexcility/utils.ex
@@ -1,9 +1,9 @@
 defmodule Flexcility.Utils do
-	def get_resource_name(resource) do
-		to_string(resource)
-		|> String.split(".")
-		|> List.last
-	end
+  def get_resource_name(resource) do
+    to_string(resource)
+    |> String.split(".")
+    |> List.last
+  end
 
   def get_properties(map, resource) do
     resource_name = get_resource_name(resource) |> String.downcase
