71a71f80d98fe812b09ad3c4872955dbebde57b5
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-24 01:17:29 +0800

    Add success: true in SessionView json response

--- apps/flexcility_web/lib/flexcility_web/views/session_view.ex
+++ apps/flexcility_web/lib/flexcility_web/views/session_view.ex
@@ -2,12 +2,11 @@ defmodule Flexcility.Web.SessionView do
   use Flexcility.Web, :view
   alias Flexcility.Web.SessionView
 
-  def render("index.json", %{sessions: sessions}) do
-    %{data: render_many(sessions, SessionView, "session.json")}
-  end
-
   def render("show.json", %{session: session}) do
-    %{data: render_one(session, SessionView, "session.json")}
+    %{
+      success: true,
+      data: render_one(session, SessionView, "session.json")
+    }
   end
 
   def render("session.json", %{session: session}) do
