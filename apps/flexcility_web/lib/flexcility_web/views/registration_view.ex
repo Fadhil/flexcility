fa3d55772cf40f75cca34b15cb9dc7e0e8448d95
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-24 01:09:11 +0800

    Add success: true to RegistrationView json response

--- apps/flexcility_web/lib/flexcility_web/views/registration_view.ex
+++ apps/flexcility_web/lib/flexcility_web/views/registration_view.ex
@@ -3,17 +3,17 @@ defmodule Flexcility.Web.RegistrationView do
   alias Flexcility.Web.RegistrationView
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, RegistrationView, "registration.json")}
+    %{success: true, data: render_many(users, RegistrationView, "registration.json")}
   end
 
   def render("show.json", %{registration: registration}) do
-    %{data: render_one(registration, RegistrationView, "registration.json")}
+    %{success: true, data: render_one(registration, RegistrationView, "registration.json")}
   end
 
   def render("registration.json", %{registration: registration}) do
     %{id: registration.id,
       name: registration.name,
       email: registration.email,
-      password: registration.password}
+      }
   end
 end
