8a7556744a29400c200122eb42fc55bdb0cd9aa8
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-23 23:54:14 +0800

    Use Accounts.create_user in RegistrationController

--- apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
+++ apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
@@ -7,7 +7,7 @@ defmodule Flexcility.Web.RegistrationController do
   action_fallback Flexcility.Web.FallbackController
 
   def create(conn, %{"registration" => registration_params}) do
-    with {:ok, %Registration{} = registration} <- Accounts.register(registration_params) do
+    with {:ok, %Registration{} = registration} <- Accounts.create_user(registration_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", registration_path(conn, :show, registration))
