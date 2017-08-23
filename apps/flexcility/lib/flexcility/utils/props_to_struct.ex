1656a51b75df95020afd1c0aec52a9ded2d9b587
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-23 22:24:13 +0800

    Update PropsToStruc
    * use __MODULE.__schema__(:fields) to get fields for a Module
      instead of @required_fields

--- apps/flexcility/lib/flexcility/utils/props_to_struct.ex
+++ apps/flexcility/lib/flexcility/utils/props_to_struct.ex
@@ -5,15 +5,8 @@ defmodule Flexcility.Utils.PropsToStruct do
 
       def from_props(props) do
         __MODULE__.__struct__
-        |> Changeset.cast(props, @all_fields)
-        |> Changeset.validate_required(@required_fields)
-        |> apply_id_from_props(props)
+        |> Changeset.cast(props, __MODULE__.__schema__(:fields))
         |> Changeset.apply_changes()
-      end
-
-      defp apply_id_from_props(cs, props) do
-        cs
-        |> Changeset.change(%{id: props["id"]})
       end
     end
   end
