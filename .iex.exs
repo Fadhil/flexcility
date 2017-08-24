13f871ade064e33aaf58aa601e7c84c052cf4fdc
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-24 18:54:54 +0800

    Add Organisation alias to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,5 @@
 alias Flexcility.Accounts
-alias Accounts.{User, Client, Registration}
+alias Accounts.{User, Client, Registration, Organisation}
 alias Flexcility.Admin
 alias Admin.{Site}
 alias Flexcility.Graph
