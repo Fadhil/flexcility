763b787455cf7505063e5c01978b89eea7535c9b
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-24 18:12:12 +0800

    Add belongs_to :organisation in Accounts.User

--- apps/flexcility/lib/flexcility/accounts/user.ex
+++ apps/flexcility/lib/flexcility/accounts/user.ex
@@ -7,5 +7,6 @@ defmodule Flexcility.Accounts.User do
     field :email, :string
     field :image, :string
     field :password_hash, :string
+    belongs_to :organisation, Flexcility.Accounts.Organisation
   end
 end
