cb2645f6b5c6aa70c5441eb2f2e291e7c59eec2a
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-08-27 13:03:19 +0800

    Update registration_controller to use Accounts.register_user

--- apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
+++ apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
@@ -6,8 +6,8 @@ defmodule Flexcility.Web.RegistrationController do
 
   action_fallback Flexcility.Web.FallbackController
 
-  def create(conn, %{"registration" => registration_params}) do
-    with {:ok, %User{} = registration} <- Accounts.create_user(registration_params) do
+  def create(conn, %{"user" => registration_params}) do
+    with {:ok, %User{} = registration} <- Accounts.register_user(registration_params) do
       conn
       |> put_status(:created)
       |> render("show.json", registration: registration)
