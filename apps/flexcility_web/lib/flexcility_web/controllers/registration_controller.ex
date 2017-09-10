e64506854f1d2f0ee74a21c740559b57671406cc
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-09-10 21:43:48 +0800

    Update RegistrationController to use %Registration{} struct

--- apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
+++ apps/flexcility_web/lib/flexcility_web/controllers/registration_controller.ex
@@ -1,7 +1,7 @@
 defmodule Flexcility.Web.RegistrationController do
   use Flexcility.Web, :controller
   alias Flexcility.Accounts
-  alias Flexcility.Accounts.User
+  alias Accounts.{Registration, Organisation}
 
   action_fallback Flexcility.Web.FallbackController
 
@@ -9,7 +9,7 @@ defmodule Flexcility.Web.RegistrationController do
     org_params = %{
       "name" => "JKR", "location" => "Wilayah Persekutuan", "address" => "KL", "subdomain" => "jkr"
     }
-    with {:ok, %User{} = registration} <- Accounts.register_user_with_org(
+    with {:ok, [registration = %Registration{}, organisation = %Organisation{}]} <- Accounts.register_user_with_org(
      registration_params, org_params
     ) do
       conn
