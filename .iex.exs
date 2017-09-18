aa9ac92dc8a5657e8a4bfe45dbecfb78f8910b51
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-09-18 20:04:49 +0800

    Add alias Graph.Query to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -3,5 +3,6 @@ alias Accounts.{User, Client, Registration, Organisation, Role}
 alias Flexcility.Admin
 alias Admin.{Site}
 alias Flexcility.Graph
+alias Graph.{Query}
 alias Flexcility.Utils
 alias Bolt.Sips, as: Bolt
